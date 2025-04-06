9675fdc6f55ab60423e0247c12370836d1db8e32
Eric Mshila <emshila19@gmail.com>
2025-04-06 08:34:29 +0300

    day0 - day 5

---
+++ Hackerank/30_Days_Of_Code/Day1_Data_Types.py
@@ -0,0 +1,24 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+second_i = int(input())
+second_d = float(input())
+second_s = input()
+
+print(i + second_i)
+print(d + second_d)
+print(s + second_s)
