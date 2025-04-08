fbf072a2cafa4c2b1c0b0e63c4236acafa0e8809
Eric Mshila <emshila19@gmail.com>
2025-04-08 17:29:27 +0300

    Thinking & Testing: A and B

---
+++ CodeWars/Thinking_&_Testing_AandB.py
@@ -0,0 +1,20 @@
+# No Story
+
+# No Description
+
+# Only by Thinking and Testing
+
+# Look at result of testcase, guess the code!
+
+
+
+def testit(a,b):
+    return a|b
+
+# test.assert_equals(testit(0, 1), 1)
+# test.assert_equals(testit(1, 2), 3)
+# test.assert_equals(testit(10, 20), 30)
+
+# a * b ?
+# test.assert_equals(testit(1, 1), 1)
+# test.assert_equals(testit(1, 3), 3)
